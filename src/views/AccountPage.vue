65c7b40e93a954a7cb89b59c99c4eef4e79bf3e1
l050101 <l01062506156@gmail.com>
2022-06-08 14:29:33 +0900

    fix

--- src/views/AccountPage.vue
+++ src/views/AccountPage.vue
@@ -1,47 +1,51 @@
 <template>
-  <ion-card>
-    <ion-card-header>
-      <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-      <ion-card-title>Card Title</ion-card-title>
-    </ion-card-header>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-card>
+        <ion-card-header>
+          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+          <ion-card-title>Card Title</ion-card-title>
+        </ion-card-header>
 
-    <ion-card-content>
-      Keep close to Nature's heart... and break clear away, once in awhile,
-      and climb a mountain or spend a week in the woods. Wash your spirit clean.
-    </ion-card-content>
-  </ion-card>
+        <ion-card-content>
+          Keep close to Nature's heart... and break clear away, once in awhile,
+          and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </ion-card-content>
+      </ion-card>
 
-  <ion-card>
-    <ion-item href="#" class="ion-activated">
-      <ion-icon :icon="wifi"></ion-icon>
-      <ion-label>Card Link Item 1 activated</ion-label>
-    </ion-item>
+      <ion-card>
+        <ion-item href="#" class="ion-activated">
+          <ion-icon :icon="wifi"></ion-icon>
+          <ion-label>Card Link Item 1 activated</ion-label>
+        </ion-item>
 
-    <ion-item href="#">
-      <ion-icon :icon="wine"></ion-icon>
-      <ion-label>Card Link Item 2</ion-label>
-    </ion-item>
+        <ion-item href="#">
+          <ion-icon :icon="wine"></ion-icon>
+          <ion-label>Card Link Item 2</ion-label>
+        </ion-item>
 
-    <ion-item class="ion-activated">
-      <ion-icon :icon="warning"></ion-icon>
-      <ion-label>Card Button Item 1 activated</ion-label>
-    </ion-item>
+        <ion-item class="ion-activated">
+          <ion-icon :icon="warning"></ion-icon>
+          <ion-label>Card Button Item 1 activated</ion-label>
+        </ion-item>
 
-    <ion-item>
-      <ion-icon :icon="walk"></ion-icon>
-      <ion-label>Card Button Item 2</ion-label>
-    </ion-item>
-  </ion-card>
+        <ion-item>
+          <ion-icon :icon="walk"></ion-icon>
+          <ion-label>Card Button Item 2</ion-label>
+        </ion-item>
+      </ion-card>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonItem, IonLabel } from '@ionic/vue';
+import { IonContent,IonPage,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonItem, IonLabel } from '@ionic/vue';
 import { pin, walk, warning, wifi, wine } from 'ionicons/icons';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'AccountPage',
-  components: { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonItem, IonLabel },
+  components: { IonContent,IonPage, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonItem, IonLabel },
   setup() {
     return {  pin, walk, warning, wifi, wine };
   }
--- src/views/UploadPage.vue
+++ src/views/UploadPage.vue
@@ -1,54 +1,57 @@
 <template>
-  <ion-card class="card-margin">
-    <ion-input value="01" readonly class="q-number"></ion-input>
-      <ion-item>
-        <ion-label position="floating">무엇을 도와드릴까요?</ion-label>
-        <ion-input clear-input value=""></ion-input>
-      </ion-item>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <br>
+      <ion-card class="card-margin">
+        <ion-input value="01" readonly class="q-number"></ion-input>
+          <ion-item>
+            <ion-label position="floating">무엇을 도와드릴까요?</ion-label>
+            <ion-input clear-input value=""></ion-input>
+          </ion-item>
 
-  </ion-card>
+      </ion-card>
+      <ion-card class="card-margin">
+          <!-- Readonly input -->
+          <ion-input value="02" readonly class="q-number"></ion-input>
+          
+          <ion-item>
+            <ion-label position="floating">이유는 무엇인가요?</ion-label>
+            <ion-input clear-input value=""></ion-input>
+          </ion-item>
+      </ion-card>
 
+      <ion-card class="card-margin">
+          <!-- Readonly input -->
+          <ion-input value="03-1" readonly class="q-number"></ion-input>
+          
+          <ion-item>
+            <ion-label position="floating">해야 할 일을 자세하게 적어주세요</ion-label>
+            <ion-input clear-input value=""></ion-input>
+          </ion-item>
+      </ion-card>
 
-    
-  <ion-card class="card-margin">
-      <!-- Readonly input -->
-      <ion-input value="02" readonly class="q-number"></ion-input>
+      <ion-card class="card-margin">
+          <!-- Readonly input -->
+          <ion-input value="03-2" readonly class="q-number"></ion-input>
+          
+          <ion-item>
+            <ion-label position="floating">심부름 값은 얼마인가요?</ion-label>
+            <ion-input clear-input value=""></ion-input>
+          </ion-item>
+      </ion-card>
       
-      <ion-item>
-        <ion-label position="floating">이유는 무엇인가요?</ion-label>
-        <ion-input clear-input value=""></ion-input>
-      </ion-item>
-  </ion-card>
-
-  <ion-card class="card-margin">
-      <!-- Readonly input -->
-      <ion-input value="03-1" readonly class="q-number"></ion-input>
       
-      <ion-item>
-        <ion-label position="floating">해야 할 일을 자세하게 적어주세요</ion-label>
-        <ion-input clear-input value=""></ion-input>
-      </ion-item>
-  </ion-card>
-
-  <ion-card class="card-margin">
-      <!-- Readonly input -->
-      <ion-input value="03-2" readonly class="q-number"></ion-input>
-      
-      <ion-item>
-        <ion-label position="floating">심부름 값은 얼마인가요?</ion-label>
-        <ion-input clear-input value=""></ion-input>
-      </ion-item>
-  </ion-card>
-
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
-import { IonLabel, IonInput, IonItem, IonCard } from '@ionic/vue';
+import { IonContent,IonPage,IonLabel, IonInput, IonItem, IonCard } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'UploadPage',
-  components: { IonLabel, IonInput, IonItem,IonCard }
+  components: { IonContent,IonPage,IonLabel, IonInput, IonItem,IonCard }
 });
 </script>
 <style>
