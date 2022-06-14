fe03291cf2d99b2fcb4806ed7fc4e634fd9a5862
l050101 <l01062506156@gmail.com>
2022-06-15 01:40:27 +0900

    back to close app

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,58 @@
 <template>
   <ion-app>
     <ion-router-outlet />
+    <ion-alert
+    :is-open="isOpenRef"
+    header="정말로 앱을 종료하시겠습니까?"
+    message=" '네'를 누르면 앱이 종료됩니다."
+    css-class="my-custom-class"
+    :buttons="buttons"
+    @didDismiss="setOpen(false)"
+    />  
   </ion-app>
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonAlert, IonApp, IonRouterOutlet } from '@ionic/vue';
 // import { defineComponent } from 'vue';
-import { useBackButton, useIonRouter } from '@ionic/vue';
+import { ref } from 'vue';
+import { useBackButton } from '@ionic/vue';
 import { Plugins } from '@capacitor/core';
 const { App } = Plugins;
 export default{
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet
+    IonRouterOutlet,
+    IonAlert,
   },
   setup(){
-    const ionRouter = useIonRouter();
-    useBackButton(-1, () => {
-      //시간 주고 2초안에 다시 클릭하면 종료하는 코드 짜야 함.
-      if (!ionRouter.canGoBack()) {
-        App.exitApp();
-      }
+    useBackButton(10, () => {
+      // 뒤로가기 물리버튼
+      setOpen(true)
     });
+
+    const isOpenRef = ref(false);
+    const setOpen = (state: boolean) => isOpenRef.value = state;
+    const buttons = [
+      {
+        text: '네',
+        handler() {
+          //네 라면 프로그램 종료
+          App.exitApp();
+        }
+      },
+      {
+        text: '아니오',
+        handler() {
+          setOpen(false)
+        }
+      }
+    ];
+    
+    return { buttons, isOpenRef, setOpen }
   }
 }
-// export default defineComponent({
-//   name: 'App',
-//   components: {
-//     IonApp,
-//     IonRouterOutlet
-//   },
-// });
 // ionic cap copy
 // ionic cap build android
 // ionic cap open android
